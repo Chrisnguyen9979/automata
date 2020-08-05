e0df17e679902a5d9e0e0da3e6eac6477db5feba
nguyen nguyen <chrisnguyen9979@gmail.com>
2020-08-05 13:52:03 +0700

    initial commit

---
+++ DFA.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <set>
+#include <fstream>
+
+using namespace std;
+
+class DFA {
+    private:
+        vector<char> alphabet;
+        vector<char> state;
+        set<char> accept;
+        map<pair<char, char>, char> transmission;
+    public:
+        DFA() {};
+        ~DFA() {};
+        void read(){
+            ifstream in("config.txt");
+            //Lưu lại đầu vào cũ
+            streambuf *cinbuf = std::cin.rdbuf();
+            //Chuyển hướng đầu vào từ file config
+            cin.rdbuf(in.rdbuf());
+            char temp;
+            int n_char;
+            cin >> n_char;
+            for (int i = 0; i < n_char; i++){
+                cin >> temp;
+                alphabet.push_back(temp);
+            }
+            int n_state;
+            cin >> n_state;
+            for (int i = 0; i < n_state; i++){
+                cin >> temp;
+                state.push_back(temp);
+            }
+            int n_accept;
+            cin >> n_accept;
+            for (int i = 0; i < n_accept; i++){
+                cin >> temp;
+                accept.insert(temp);
+            }
+            for (int i = 0; i < n_state; i++){
+                for (int j = 0; j < n_char; j++){
+                        cin >> temp;
+                        transmission[{state[i], alphabet[j]}] = temp;
+                }
+            }
+            //Chuyển hướng đầu vào lại như ban đầu
+            cin.rdbuf(cinbuf);
+        }
+        void print(){
+            for (int i = 0; i < state.size(); i++){
+                for (int j = 0; j < alphabet.size(); j++){
+                        cout << state[i] << " " << alphabet[j] << " " << transmission[{state[i], alphabet[j]}] << endl;
+                }
+            }
+            cout << "Accept at ";
+            for (auto q : accept){
+                cout << q << " ";
+            }
+            cout << endl;
+        }
+        bool include(string s){
+            char start = state[0];
+            for (auto c : s){
+                start = transmission[{start, c}];
+            }
+            if (accept.count(start) == 1){
+                return true;
+            }
+            return false;
+        }
+};
+int main(){
+    DFA dfa;
+    dfa.read();
+
+    int tc = 0;
+    cin >> tc; getchar();
+    while (tc--){
+        string a;
+        getline(cin, a);
+        cout << dfa.include(a) << endl;
+    }
+    return 0;
+}
